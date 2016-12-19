13253c963834a687a564dff389db60985dd9a87f
bchok <bchok91@gmail.com>
2016-12-19 09:21:25 -0500

    altered the view to display a profile picture to the left of the welcome message

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+              <img src="/uploads/pictures/{{$user->picture}}" style="width:250px; height:250px; float:left; border-radius:25%; margin-right:50px">
               <h1> Welcome! {{$user->name}} </h1>
         </div>
     </div>
