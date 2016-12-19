2ebe73d5a5ee5fc4770c7d453863d004e8e8ed34
bchok <bchok91@gmail.com>
2016-12-19 10:04:45 -0500

    added function to usercontroller to handle user uploading of image. It utilized the image intervention package found here http://image.intervention.io/

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,26 @@ use finalproject\Http\Requests;
 
 use Auth;
 
+use Image;
+
 class UserController extends Controller
 {
     //
     public function profile(){
       return view('profile', array('user' => Auth::user()));
+    }
+
+    public function update_pic(Request $request){
+      if($request->hasFile('picture')){
+        $picture = $request->file('picture');
+        $filename = time() . '.' . $picture->getClientOriginalExtension();
+        Image::make($picture)->resize(250, 250)->save(public_path('/uploads/pictures/' . $filename));
+
+        $user = Auth::user();
+        $user->picture = $filename;
+        $user->save();
+      }
+      return view('profile', array('user' => Auth::user()));
+
     }
 }
