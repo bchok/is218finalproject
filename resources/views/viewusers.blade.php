166bf81e85f8924fefc922f59d9221162f5564be
bchok <bchok91@gmail.com>
2016-12-19 16:13:54 -0500

    updated viewusers view to display table

--- resources/views/viewusers.blade.php
+++ resources/views/viewusers.blade.php
@@ -4,7 +4,12 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-              TEST!
+              <table border="2">
+                  <th> Registered Users </th>
+                @foreach($users as $user)
+                  <tr><td> {{$user->name}} </td></tr>
+                @endforeach
+            </table>
         </div>
     </div>
 </div>
