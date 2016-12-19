c53962ab20e32c20de4a4e6ac93b9932d5be94bc
bchok <bchok91@gmail.com>
2016-12-19 11:48:00 -0500

    neatened up my edit profile view

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -4,19 +4,22 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-              <form action="/editprofile" method="POST">
+              <form action="/editprofile" name="updateemail" method="POST">
                 &nbsp;&nbsp;<label> Update Email </label>
-                <input type="text" name="updateemail" style="float:left"><br><br>
+                <input type="text" name="newemail" style="float:left"><br><br>
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
                 <input type="submit" class="btn btn-sm">
               </form>
-              <form action="/editprofile" method="POST"><br>
+              <form action="/editprofile" name="updatepass" method="POST"><br>
                 &nbsp;&nbsp;<label> Update Password </label>
                 <input type="text" name="updatepass" style="float:left"><br><br>
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
                 <input type="submit" class="btn btn-sm">
               </form>
-              <form action="/editprofile" method="POST"><br>
+              <form action="/editprofile" name="updatename" method="POST"><br>
                 &nbsp;&nbsp;<label> Update Name </label>
                 <input type="text" name="updatename" style="float:left"><br><br>
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
                 <input type="submit" class="btn btn-sm">
               </form>
         </div>
