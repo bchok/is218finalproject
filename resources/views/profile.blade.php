8434349943a4cc4d8f9432ff2473581c92dde4fe
bchok <bchok91@gmail.com>
2016-12-19 09:34:29 -0500

    added the form html for file uploads for the profile picture

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,6 +6,12 @@
         <div class="col-md-10 col-md-offset-1">
               <img src="/uploads/pictures/{{$user->picture}}" style="width:250px; height:250px; float:left; border-radius:25%; margin-right:50px">
               <h1> Welcome! {{$user->name}} </h1>
+              <form enctype="multipart/form-data" action="/profile" method="POST">
+                <label> Change Profile Picture </label>
+                <input type="file" name="picture">
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                <input type="submit" class="pull-right btn btn-md btn-primary">
+              </form>
         </div>
     </div>
 </div>
