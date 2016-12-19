c2212079ef6e2e685f7ac14ece024ccfcfbfbbcd
bchok <bchok91@gmail.com>
2016-12-19 10:17:49 -0500

    added the default layout page to display the user profile picture next to the users log in name on the top right

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,8 @@
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="position:relative; padding-left:40px">
+                                <img src="/uploads/pictures/{{Auth::user()->picture}}" style="width:25px; height:25px; position:absolute; top:10px; left:10px; border-radius:25%">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
