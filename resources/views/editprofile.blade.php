83ec37d04caf5406525f817823beba3869cbc6ad
bchok <bchok91@gmail.com>
2016-12-19 11:08:03 -0500

    added the form html to my profile template

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -6,17 +6,17 @@
         <div class="col-md-10 col-md-offset-1">
               <form action="/editprofile" method="POST">
                 &nbsp;&nbsp;<label> Update Email </label>
-                <input type="text" name="updateemail" style="float:left"><br>
+                <input type="text" name="updateemail" style="float:left"><br><br>
                 <input type="submit" class="btn btn-sm">
               </form>
               <form action="/editprofile" method="POST"><br>
                 &nbsp;&nbsp;<label> Update Password </label>
-                <input type="text" name="updatepass" style="float:left"><br>
+                <input type="text" name="updatepass" style="float:left"><br><br>
                 <input type="submit" class="btn btn-sm">
               </form>
               <form action="/editprofile" method="POST"><br>
                 &nbsp;&nbsp;<label> Update Name </label>
-                <input type="text" name="updatename" style="float:left"><br>
+                <input type="text" name="updatename" style="float:left"><br><br>
                 <input type="submit" class="btn btn-sm">
               </form>
         </div>
