1cb6f54a0218a87000eee15913e4454efe822393
bchok <bchok91@gmail.com>
2016-12-19 09:37:33 -0500

    added a post route that points to a usercontroller for updating the profile pic

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 Route::get('profile', 'UserController@profile');
 
+Route::post('profile', 'UserController@update_pic');
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
