18630c65550a14ad4d41ae68b9b346a91fb086b6
bchok <bchok91@gmail.com>
2016-12-19 18:17:47 -0500

    last minute changes to make table look nicer

--- resources/views/viewusers.blade.php
+++ resources/views/viewusers.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-              <table border="2">
-                  <th> Registered Users </th>
+              <table border="2" align="center" width="75%">
+                  <th style="text-align:center"> Registered Users </th>
                 @foreach($users as $user)
-                  <tr><td> {{$user->name}} </td></tr>
+                  <tr><td style="text-align:center"> {{$user->name}} </td></tr>
                 @endforeach
             </table>
         </div>
